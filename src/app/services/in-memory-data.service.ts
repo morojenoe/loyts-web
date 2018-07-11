25bd31b9751fd7821825a8b6e6924c42c654edf3
Dmitry Sutygin <dmitry.sutygin@gmail.com>
2018-07-11 11:54:47 +0400

    updated layout of explore component

--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -1,22 +1,24 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Application } from '../models/application';
 import { Proposal } from '../models/proposal';
+import { addMinutes } from 'date-fns';
 
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
+    const appDate = new Date(Date.now());
     const applications = [
-      new Application(1, 'english', ['music', 'rock', 'alternative'], new Date(Date.now()), 3, null),
+      new Application(1, 'english', ['music', 'rock', 'alternative'], appDate, 3, null),
       new Application(2, 'english',
                       ['sport', 'football', 'FIFA World Cup 2018', 'disappointment', 'prediction'],
-                      new Date(Date.now()), 5, null),
-      new Application(3, 'english', ['hollywood walk of fame'], new Date(Date.now()), 22, null),
-      new Application(4, 'chinese', ['culture', 'street food'], new Date(Date.now()), 8, null),
-      new Application(5, 'french', ['paris', 'attraction', 'landmark'], new Date(Date.now()), 31, null),
-      new Application(6, 'french', [], new Date(Date.now()), 19, null),
-      new Application(7, 'german', [], new Date(Date.now()), 20, null),
-      new Application(8, 'german', ['politics', 'election'], new Date(Date.now()), 11, null),
-      new Application(9, 'chinese', ['tell me about shanghai'], new Date(Date.now()), 15, null),
-      new Application(10, 'french', ['everything'], new Date(Date.now()), 66, null),
+                      addMinutes(appDate, 2), 5, null),
+      new Application(3, 'english', ['hollywood walk of fame'], addMinutes(appDate, 10), 22, null),
+      new Application(4, 'chinese', ['culture', 'street food'], addMinutes(appDate, 30), 59, null),
+      new Application(5, 'french', ['paris', 'attraction', 'landmark'], addMinutes(appDate, 59), 31, null),
+      new Application(6, 'french', [], addMinutes(appDate, 60), 18, null),
+      new Application(7, 'german', [], addMinutes(appDate, 80), 3, null),
+      new Application(8, 'german', ['politics', 'election'], addMinutes(appDate, 100), 11, null),
+      new Application(9, 'chinese', ['tell me about shanghai'], addMinutes(appDate, 120), 15, null),
+      new Application(10, 'french', ['everything'], addMinutes(appDate, 28), 66, null),
     ];
     const proposals: Proposal[] = [
       {
